d4cc44b224de444939da18155f0afe5748e99d27
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-28 15:55:33 +0100

    Syncify tests

--- test/test.js
+++ test/test.js
@@ -3,60 +3,51 @@
 var Duo = require('duo');
 var path = require('path');
 var fixture = path.join.bind(path, __dirname, 'fixtures');
-var read = require('fs').readFile;
+var read = require('fs').readFileSync;
 var sass = require('../');
 var test = require('ava');
 
 test('should transpile sass to css', function (t) {
-  t.plan(3);
+  t.plan(2);
 
   var root = fixture('simple');
+  var expected = read(fixture('simple/expected.css'), 'utf8');
   var duo = new Duo(root)
     .entry('index.scss')
     .use(sass());
 
-  duo.run(function (err, a) {
+  duo.run(function (err, css) {
     t.assert(!err, err);
-
-    read(path.join(root, 'expected.css'), 'utf8', function (err, b) {
-      t.assert(!err, err);
-      t.assert(a === b);
-    });
+    t.assert(css === expected);
   });
 });
 
 test('should transpile sass to css using indented syntax', function (t) {
-  t.plan(3);
+  t.plan(2);
 
   var root = fixture('indented');
+  var expected = read(fixture('indented/expected.css'), 'utf8');
   var duo = new Duo(root)
     .entry('index.sass')
     .use(sass());
 
-  duo.run(function (err, a) {
+  duo.run(function (err, css) {
     t.assert(!err, err);
-
-    read(path.join(root, 'expected.css'), 'utf8', function (err, b) {
-      t.assert(!err, err);
-      t.assert(a === b);
-    });
+    t.assert(css === expected);
   });
 });
 
 test('should transpile sass with imports to css', function (t) {
-  t.plan(3);
+  t.plan(2);
 
   var root = fixture('import');
+  var expected = read(fixture('import/expected.css'), 'utf8');
   var duo = new Duo(root)
     .entry('index.scss')
     .use(sass());
 
-  duo.run(function (err, a) {
+  duo.run(function (err, css) {
     t.assert(!err, err);
-
-    read(path.join(root, 'expected.css'), 'utf8', function (err, b) {
-      t.assert(!err, err);
-      t.assert(a === b);
-    });
+    t.assert(css === expected);
   });
 });
