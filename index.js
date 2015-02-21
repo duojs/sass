59961b22d782c6ff5768a7bbe3a8796976a6ee2a
Kevin Martensson <kevinmartensson@gmail.com>
2015-02-21 17:31:53 +0100

    Tweaks

--- index.js
+++ index.js
@@ -4,30 +4,22 @@ var assign = require('object-assign');
 var debug = require('debug')('duosass');
 var sass = require('node-sass').renderSync;
 
-/**
- * Compile Sass to CSS
- *
- * @param {Object} [opts]
- * @api public
- * @return {Function}
- */
-
 module.exports = function (opts) {
-  opts = opts || {};
+	opts = opts || {};
 
-  return function (file) {
-    if (file.type !== 'scss' && file.type !== 'sass') {
-      return;
-    }
+	return function (file) {
+		if (file.type !== 'scss' && file.type !== 'sass') {
+			return;
+		}
 
-    debug('compiling %s to css', file.id);
+		debug('compiling %s to css', file.id);
 
-    file.src = sass(assign({
-      data: file.src,
-      includePaths: [file.root],
-      indentedSyntax: file.type === 'sass'
-    }, opts));
+		file.src = sass(assign({
+			data: file.src,
+			includePaths: [file.root],
+			indentedSyntax: file.type === 'sass'
+		}, opts));
 
-    file.type = 'css';
-  };
+		file.type = 'css';
+	};
 };
