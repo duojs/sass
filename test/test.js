882b49ba0988efc7a367871557d13e670ba390ca
Kevin Martensson <kevinmartensson@gmail.com>
2015-03-27 09:38:32 +0100

    Tweaks

--- test/test.js
+++ test/test.js
@@ -1,53 +1,53 @@
 'use strict';
 
-var Duo = require('duo');
-var path = require('path');
-var fixture = path.join.bind(path, __dirname, 'fixtures');
 var read = require('fs').readFileSync;
-var sass = require('../');
+var path = require('path');
 var test = require('ava');
+var Duo = require('duo');
+var sass = require('../');
+var fixture = path.join.bind(path, __dirname, 'fixtures');
 
 test('should transpile sass to css', function (t) {
-  t.plan(2);
+	t.plan(2);
 
-  var root = fixture('simple');
-  var expected = read(fixture('simple/expected.css'), 'utf8');
-  var duo = new Duo(root)
-    .entry('index.scss')
-    .use(sass());
+	var root = fixture('simple');
+	var expected = read(fixture('simple/expected.css'), 'utf8');
+	var duo = new Duo(root)
+		.entry('index.scss')
+		.use(sass());
 
-  duo.run(function (err, css) {
-    t.assert(!err, err);
-    t.assert(css === expected);
-  });
+	duo.run(function (err, css) {
+		t.assert(!err, err);
+		t.assert(css === expected);
+	});
 });
 
 test('should transpile sass to css using indented syntax', function (t) {
-  t.plan(2);
+	t.plan(2);
 
-  var root = fixture('indented');
-  var expected = read(fixture('indented/expected.css'), 'utf8');
-  var duo = new Duo(root)
-    .entry('index.sass')
-    .use(sass());
+	var root = fixture('indented');
+	var expected = read(fixture('indented/expected.css'), 'utf8');
+	var duo = new Duo(root)
+		.entry('index.sass')
+		.use(sass());
 
-  duo.run(function (err, css) {
-    t.assert(!err, err);
-    t.assert(css === expected);
-  });
+	duo.run(function (err, css) {
+		t.assert(!err, err);
+		t.assert(css === expected);
+	});
 });
 
 test('should transpile sass with imports to css', function (t) {
-  t.plan(2);
+	t.plan(2);
 
-  var root = fixture('import');
-  var expected = read(fixture('import/expected.css'), 'utf8');
-  var duo = new Duo(root)
-    .entry('index.scss')
-    .use(sass());
+	var root = fixture('import');
+	var expected = read(fixture('import/expected.css'), 'utf8');
+	var duo = new Duo(root)
+		.entry('index.scss')
+		.use(sass());
 
-  duo.run(function (err, css) {
-    t.assert(!err, err);
-    t.assert(css === expected);
-  });
+	duo.run(function (err, css) {
+		t.assert(!err, err);
+		t.assert(css === expected);
+	});
 });
